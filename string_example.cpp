e50ce1f552df16bb79616466a0a5d3110c9550a6
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 22:23:36 +0530

    initial commit

---
+++ string_example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+  string example="hello dear welcome to class";
+  cout <<"Example of string -  "<<example;
+  return 1;
+}
