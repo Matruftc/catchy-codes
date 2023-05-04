a5f86fba3825e5b9c679be60056464eed0bee1fd
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 22:00:29 +0530

    initial commit

---
+++ cube.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int usernumber;
+    cout<<"Took a number dear: ";
+    cin>> usernumber;
+    int cube_of_numbers=usernumber*usernumber*usernumber;
+    cout <<"cube of the number is :   "<<cube_of_numbers;
+    return 0;
+}
