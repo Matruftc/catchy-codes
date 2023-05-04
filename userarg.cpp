589a082348189147eb3cc24c67072793a769ca71
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 21:52:36 +0530

    initial commit

---
+++ userarg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int usernumber;
+    int usernumber1;
+    cout<<"took a number dear:  ";
+    cin>>usernumber;
+    cout<<"took the second number dear:  ";
+    cin>>usernumber1;
+    int result_of_two_numbers=usernumber+usernumber1;
+    cout <<"result of two number is :  "<<result_of_two_numbers;
+    return 0;
+}
