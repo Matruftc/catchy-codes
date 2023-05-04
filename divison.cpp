118e08a87c6ffc4c98439cfc060f2865ce83d260
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 21:49:00 +0530

    initial commit

---
+++ divison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int usernumber=10;
+    int usernumber1=2;
+    int result_of_two_numbers=usernumber%usernumber1;
+    int result_of_two_numbers1=usernumber/usernumber1;
+    cout <<"\n";
+    cout <<"Remainder of two number is :   "<<result_of_two_numbers;
+    cout <<"\n";
+    cout <<"quotient of two number is :    "<<result_of_two_numbers1;
+    return 0;
+}
