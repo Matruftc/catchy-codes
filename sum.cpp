287c3112f15d93eb12978a54676c8cb19279c039
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 21:46:41 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int usernumber=10;
+    int usernumber1=20;
+    int result_of_two_numbers=usernumber+usernumber1;
+    cout <<"result of two number is :   "<<result_of_two_numbers;
+    return 0;
+}
