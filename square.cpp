1a3d17c1fb44abc6cb8b641cdfb0bafdbf9b3266
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-04 21:58:44 +0530

    initial commit

---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int usernumber;
+    cout<<"Took a number dear: ";
+    cin>> usernumber;
+    int square_of_numbers=usernumber*usernumber;
+    cout <<"square of two number is :   "<<square_of_numbers;
+    return 0;
+}
