20e21bcb2798b6c88ed9a64568a3a39b33ef9c9b
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-05 22:44:22 +0530

    initial commit

---
+++ newline.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{  
+    cout<<"hello dear i am the line number 1   ";
+    cout<<"this line going to collapse with line number 1";
+
+    cout<<"\n i am a new line property";
+    cout<<"\t i am a tab property";
+
+    return 0;
+}
