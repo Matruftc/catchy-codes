687bba534dc4e2670eb5de048209f00c891f8d65
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-05 22:52:09 +0530

    initial commit

---
+++ comment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{  
+    /*
+    cout<<"\n i am a new line property ";
+    cout<<"\t i am a tab property";
+    cout<<"\t i am the second tab property";
+    cout<<"\t i am the third tab property";
+    */
+    return 0;
+}
