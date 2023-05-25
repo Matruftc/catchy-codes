12e7a61154d8b3b99701430b03dc12017f9feb0d
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-25 10:32:43 +0530

    initial commit

---
+++ string2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+ string firstName = "Priya";
+ string lastName = " Mishra";
+ string fullName = firstName.append(lastName);
+ cout << fullName;
+}
