7a8a80a3f2b0dd9f2f68c82510df8aff5a4e0a9d
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-25 10:43:54 +0530

    initial commit

---
+++ string_index1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//#include<string>
+
+using namespace std;
+
+int main()
+{
+
+    string name = "ronny";
+    cout<<name[0];
+    cout<<name[1];
+   
+    name[4] = 'i';
+
+    cout<<"\n"<<name;
+
+}    
