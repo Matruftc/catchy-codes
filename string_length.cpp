f65d60d6fae1f56fb1334dcca44f963a52302369
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-25 10:39:12 +0530

    initial commit

---
+++ string_length.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+//#include<string>
+
+using namespace std;
+
+int main()
+{
+  string length_of_my_name = "Matru Prasad Mishra";
+  cout <<"the length of my name is :"<<length_of_my_name.length();
+  return 0;
+}   
