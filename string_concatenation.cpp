f24b2a1776535454760c9570eb70583d767f1b05
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-25 10:30:45 +0530

    initial commit

---
+++ string_concatenation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+ string firstName = "Priya";
+ string lastName = "Mishra";
+ string fullName = firstName + lastName;
+ cout << fullName;
+}
