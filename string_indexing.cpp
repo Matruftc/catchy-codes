8738e4b987b6a0f1f6ca62be7be422f5ab5b9519
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-25 10:41:55 +0530

    initial commit

---
+++ string_indexing.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//#include<string>
+
+using namespace std;
+
+int main()
+{
+
+    string name = "ronny";
+    cout<<name[0];
+    cout<<name[1];
+    cout<<name[2];
+    cout<<name[3];
+    cout<<name[4];
+    //cout<<name[5]; useless
+    //cout<<name[6]; useless
+
+}    
