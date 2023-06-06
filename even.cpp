5f38e91a16da6c142463cdf6d6b916adf6480a74
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 11:29:32 +0530

    initial commit

---
+++ even.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int num1=0;
+int range;
+
+int main()
+{
+    cout<<"put your range: ";
+    cin>>range;
+
+    while(num1<range)
+   {
+      cout<<"\n"<<2*num1;
+      num1++;
+
+}
+}
