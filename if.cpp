d4066246f6f45f823022a201eaeba91a49e2c22b
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 11:04:04 +0530

    initial commit

---
+++ if.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int num1,num2;
+
+int main()
+{
+  cout<<"PLease give a random number:  ";
+  cin>>num1;
+
+  cout<<"PLease give a random number:  ";
+  cin>>num2;
+
+  if(num1>num2)
+  {
+    cout<<"num1 is greater than  num2";
+  }
+  else
+  {
+    cout<<"num2 is greater than num1";
+  }
+}
