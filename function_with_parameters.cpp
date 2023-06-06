a0e931663e242c5188d9e747ef25fc4eb4d536fc
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 19:25:40 +0530

    initial commit

---
+++ function_with_parameters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+void show(int num1,float num2) {
+ cout<<"\ni am a integer: "<<num1;
+ cout<<"\ni am a float: "<<num2;
+}
+
+int main() {
+
+  int n1=10;
+  float n2=12.34;
+
+  show(n1,n2);
+
+}
