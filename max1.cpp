264c11bce09a216176f6f15b3fb11a0131dff8b5
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 10:40:41 +0530

    initial commit

---
+++ max1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+using namespace std;
+
+int num1,num2;
+int main()
+{
+  cout<<"PLease give a random number";
+  cin>>num1;
+
+  cout<<"PLease give a random number";
+  cin>>num2;
+
+  cout<<max(num1,num2);
+  
+}  
