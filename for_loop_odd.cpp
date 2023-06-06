b0896f91a7b38e7d44d18e0e8ca8f09884707d13
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 11:38:36 +0530

    initial commit

---
+++ for_loop_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int num=0;
+int range;
+
+int main()
+{
+    cout<<"PLease give a random number:  ";
+    cin>>range;
+
+    for( num=0;num<range;num++)
+    {
+         cout<<"\nvalue of num:  "<<(2*num)+1;
+    }
+}
