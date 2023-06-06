cb5264d60531656418facfa7528f78e73c93c6de
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 11:03:51 +0530

    initial commit

---
+++ if1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int num1=10,num2=20;
+
+int main()
+{
+ if(num1>num2)
+  {
+    cout<<"num1 is greater than  num2";
+  }
+  else
+  {
+    cout<<"num2 is greater than num1";
+  }
+}
