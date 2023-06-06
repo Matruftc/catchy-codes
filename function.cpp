2e480ed87e4fc93e625be8b5ee63a26bbebd7741
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-06 19:21:20 +0530

    initial commit

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+ 
+void greet()        // i was just create the function  
+{
+    cout<<"\n Hello dear how are you";
+}
+
+int main()
+{
+    greet();   // i am just calling out the function
+}
