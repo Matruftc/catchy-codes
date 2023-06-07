58af3c73fcaf5c510a09ccd2f4a55690ca14f173
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-08 01:28:06 +0530

    initial commit

---
+++ var.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+  int n1,n2;
+  cout<<"enter the number: ";
+  // cin>>n1>>"\t">>n2;  this line contains errors
+   cin>>n1>>n2;
+
+   // or you can write this
+
+   // cin>>n1,n2;
+
+   
+  cout<<"\t"<<n1<<"\t"<<n2;
+
+}
