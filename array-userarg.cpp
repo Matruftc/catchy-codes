baa741839ba7e0ae9010fc219cc1f763bd49d8b7
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-23 12:47:47 +0530

    initial commit

---
+++ array-userarg.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+   int marks[5];
+
+   for(int i=0;i<marks[5];i++)
+   {
+    cout<<"\nEnter your marks";
+    cin>>marks[i];
+   }
+
+    for(int i=0;i<marks[5];i++)
+   {
+    cout<<"\nThe marks are: "<<marks[i];
+   }
+}
