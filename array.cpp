bb99eec4798a3db512ab811b7bca538c64fe90b3
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-23 12:44:46 +0530

    initial commit

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+   int marks[]={12,15,13,15,18,20};
+
+   for(int i=0;i<marks[5];i++)
+   {
+    cout<<"\nThe marks are: "<<marks[i];
+   }
+}
