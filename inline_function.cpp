5b1d822541f49e348fa0aaac0f1c0fa6c7888ac2
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-27 19:21:22 +0530

    initial commit

---
+++ inline_function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+using namespace std;  
+inline int add(int a, int b)  
+{  
+    return(a+b);  
+}  
+int main()  
+{  
+    cout<<"Addition of 'a' and 'b' is:"<<add(2,3);
+     
+  
+}  
