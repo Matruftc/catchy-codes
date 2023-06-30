d32dae381211634d1847629956d5ab8784d2e1c2
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-30 21:53:20 +0530

    initial commit

---
+++ class-teacher01.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Teacher{
+    private:
+          int roll;
+          string name;
+          int age;
+
+   public:
+      void input();
+      void show();
+};
+    void Teacher :: input() 
+            {
+                cout<<"Enter your roll no:";
+                cin>>roll;
+                
+                cout<<"Enter your name:";
+                cin>>name;
+
+                cout<<"Enter your age:";
+                cin>>age;
+            }
+
+   void Teacher :: show()
+            {
+                cout<<"\n your roll no:"<<roll;
+                cout<<"\n your roll no:"<<name;
+                cout<<"\n your roll no:"<<age;
+            }
+
+
+int main()
+{
+    Teacher s;
+    s.input();
+    s.show();
+    return 0;
+}
